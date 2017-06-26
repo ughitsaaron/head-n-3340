ce399e93f5110308e930436125ade6edab38e23e
Aaron Petcoff <hello@aaronpetcoff.me>
2017-06-26 11:46:19 -0400

    fix scheduler? wtf

--- clock.py
+++ clock.py
@@ -1,10 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from fetch_data import cron
+from worker import cron
 
 # add cron jobs and spin up the scheduler
 if __name__ == "__main__":
     sched = BlockingScheduler()
 
-    sched.add_job(cron, "cron", id="midnight", hour=16)
-    sched.add_job(cron, "cron", id="noon", hour=1)
+    sched.add_job(cron, "cron", id="noon", hour=16)
+    sched.add_job(cron, "cron", id="nine_pm", hour=1)
     sched.start()
